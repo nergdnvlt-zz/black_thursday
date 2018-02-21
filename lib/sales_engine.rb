ee7b69e287e7a0d7d4185ed7dd5a2dd335ac3609
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-21 09:04:30 -0700

    Working on sales engine

---
+++ lib/sales_engine.rb
@@ -0,0 +1,23 @@
+require 'CSV'
+require_relative 'merchant_repository'
+require_relative 'item_repository'
+
+# Creates a sales engine class to tie everything together
+class SalesEngine
+  attr_reader :item_csv,
+              :merchant_csv,
+              :items,
+              :merchants
+
+  def initialize(data)
+    require "pry"; binding.pry
+    @item_csv = data[:items]
+    @merchant_csv = data[:merchants]
+    @items = ItemRepository.new(@item_csv)
+    @merchants = MerchantRepository.new(@merchant_csv)
+  end
+
+  def self.from_csv(data)
+    self.new(data)
+  end
+end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -5,11 +5,20 @@ require_relative '../lib/sales_engine'
 
 # Testing merchant class
 class SalesEngineTest < MiniTest::Test
+  def setup
+    @data = {
+              items: './data/items.csv',
+              merchants: './data/merchants.csv'
+            }
+    @sales_engine = SalesEngine.from_csv(@data)
+  end
+
   def test_it_exists
-    sales_engine = SalesEngine.from_csv({
-                                          items: './data/items.csv',
-                                          merchants: './data/merchants.csv'
-                                        })
-    assert_instance_of SalesEngine, sales_engine
+    assert_instance_of SalesEngine, @sales_engine
+  end
+
+  def test_for_from_csv_method
+    assert_equal @data[:items], SalesEngine.from_csv(@data).item_csv_path
+    assert_equal @data[:merchants], SalesEngine.from_csv(@data).merchant_csv_path
   end
 end
