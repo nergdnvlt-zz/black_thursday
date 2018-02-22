fa5873a7380e84338f914a09f47a7bc50fcf7298
gijolopez <glopez22@me.com>
2018-02-22 08:14:23 -0700

    standard Deviation method updated

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -16,15 +16,21 @@ class SalesAnalyst
   def average_items_per_merchant
   (items.count.to_f / merchants.count).round(2)
   end
+
+  def average_items_per_merchant_standard_deviation
+    average_item_count = average_items_per_merchant
+    Math.sqrt(
+      merchants.reduce(0) do |sum, merchant|
+        sum + (merchant.items.count - average_item_count)**2
+      end / (merchants.count - 1)
+    ).round(2)
+  end
 end
 
 
 
 
 
-#
-# average_items_per_merchant
-# average_items_per_merchant_standard_deviation
 # merchants_with_high_item_count
 # average_item_price_for_merchant(12334159)
 # average_average_price_per_merchant
