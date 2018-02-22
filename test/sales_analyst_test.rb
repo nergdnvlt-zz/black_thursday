5d46d43ebd6b2152b2216fbca0b4158547fb89f8
gijolopez <glopez22@me.com>
2018-02-21 18:29:01 -0700

    making test pass

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,7 +1,27 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require_relative '../lib/sales_analyst'
+require_relative '../lib/sales_engine'
 
 require_relative '../lib/sales_analyst'
 
 class SalesAnalystTest < MiniTest::Test
+
+  def setup
+    se = SalesEngine.from_csv({
+      items:     './test/fixtures/items.csv',
+      merchants: './test/fixtures/merchants.csv'
+    })
+    @sa = SalesAnalyst.new(se)
+  end
+
+  def test_it_finds_all_the_merchants
+    merchants = @sa.merchants
+    assert_equal 9, merchants.count
+  end
+
+  def test_it_finds_all_the_items
+   items = @sa.items
+   assert_equal 1, items.count
+ end
 end
