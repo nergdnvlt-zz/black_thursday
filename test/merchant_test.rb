d28b81fa2a3170609eee9d5fe72a65c44bef3fc8
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-21 20:17:47 -0700

    Finishing relationship layer for iteration 1, closing branch.

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 require_relative '../lib/merchant'
+require_relative '../lib/sales_engine'
 
 # Testing merchant class
 class MerchantTest < MiniTest::Test
@@ -23,5 +24,17 @@ class MerchantTest < MiniTest::Test
 
     assert_equal 8, merchant.id
     assert_equal 'Hogwarts', merchant.name
+  end
+
+  def test_if_it_returns_all_items_for_a_merchant
+    data = { items: './test/fixtures/items.csv',
+             merchants: './test/fixtures/merchants.csv' }
+    sales_engine = SalesEngine.new(data)
+    id = 123_341_85
+    merchant = sales_engine.merchants.find_by_id(id)
+
+    assert merchant.items.class == Array
+    assert merchant.items.first.class == Item
+    assert_equal 3, merchant.items.length
   end
 end
