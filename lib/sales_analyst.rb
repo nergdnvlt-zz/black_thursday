ebfcc772b85d7a53fc2835bef1fbf1f252e5ea47
gijolopez <glopez22@me.com>
2018-02-22 11:41:28 -0700

    updated Sales_Analyst_test and corrected errors

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,11 +1,12 @@
 require 'bigdecimal'
 
+# This class analyzes all the data from the sales engine.
 class SalesAnalyst
   def initialize(sales_engine)
     @sales_engine = sales_engine
   end
 
-    def merchants
+  def merchants
     @sales_engine.merchants.all
   end
 
@@ -14,7 +15,7 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
-  (items.count.to_f / merchants.count).round(2)
+    (items.count.to_f / merchants.count).round(2)
   end
 
   def average_items_per_merchant_standard_deviation
@@ -27,8 +28,8 @@ class SalesAnalyst
   end
 
   def one_stdev_above_average
-   average_items_per_merchant + average_items_per_merchant_standard_deviation
- end
+    average_items_per_merchant + average_items_per_merchant_standard_deviation
+  end
 
   def merchants_with_high_item_count
     high_item_count = one_stdev_above_average
@@ -36,18 +37,18 @@ class SalesAnalyst
       merchant.items.count > high_item_count
     end
   end
-# I am having trouble with this one, can you
-# take a look at it? Thanks 
+
   def average_item_price_for_merchant(merchant_id)
-    merchant = @sales_engine.find_merchant_by_merchant_id(merchant_id)
-    return merchant.average_item_price
+    inventory = @sales_engine.merchants.find_by_id(merchant_id).items
+    prices    = inventory.map(&:unit_price)
+    # require "pry"; binding.pry
+    (prices.reduce(:+) / inventory.size).round(2)
   end
 
+  def average_average_price_per_merchant
+    total = merchants.reduce(0) do |sum, merchant|
+      sum + average_item_price_for_merchant(merchant.id)
+    end
+    (total / merchants.size).round(2)
+  end
 end
-
-
-
-
-
-# average_average_price_per_merchant
-# golden_items
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,43 +1,24 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require_relative 'helper_test'
 require_relative '../lib/sales_analyst'
 require_relative '../lib/sales_engine'
 
-class SalesAnalystTest < MiniTest::Test
-
+# Test sales analyst class
+class SalesAnalystTest < Minitest::Test
   def setup
-    se = SalesEngine.from_csv({
-      items:     './test/fixtures/items.csv',
-      merchants: './test/fixtures/merchants.csv'
-    })
-    @sa = SalesAnalyst.new(se)
+    @se = SalesEngine.from_csv(
+      items:     './data/items.csv',
+      merchants: './data/merchants.csv'
+    )
+    @sales_analyst = SalesAnalyst.new(@se)
   end
 
-  def test_it_finds_all_the_merchants
-    merchants = @sa.merchants
-    assert_equal 9, merchants.count
+  def test_average_item_price_for_merchant
+    result = @sales_analyst.average_item_price_for_merchant(12334185)
+    assert result.is_a?(BigDecimal)
   end
 
-  def test_it_finds_all_the_items
-   items = @sa.items
-   assert_equal 34, items.count
- end
-
- def test_it_finds_average_items_per_merchant
-   assert_equal 3.78, @sa.average_items_per_merchant
- end
-
- def test_it_finds_the_average_items_per_merchant_stdev
-    assert_equal 4.01, @sa.average_items_per_merchant_standard_deviation
-  end
-
-  def test_it_finds_the_merchants_with_the_highest_item_counts
-    merchants = @sa.merchants_with_high_item_count
-
-    assert_equal 0, merchants.count
-  end
-
-  def test_it_finds_average_item_price_for_merchant
-    assert_equal 00.00, @sa.average_item_price_for_merchant(12334185)
+  def test_average_average_item_price_for_merchant
+    result = @sales_analyst.average_average_price_per_merchant
+    assert result.is_a?(BigDecimal)
   end
 end
