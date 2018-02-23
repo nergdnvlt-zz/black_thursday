4008b771c99b471bfa76432c851691e217c5fd3c
gijolopez <glopez22@me.com>
2018-02-23 11:43:07 -0700

    deleted a line a the bottom on invoice repo" : end !!! "

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -41,8 +41,4 @@ class InvoiceRepository
     "#<#{self.class} #{@invoices.size} rows>"
   end
 end
-# all - returns an array of all known Invoice instances
-# find_by_id - returns either nil or an instance of Invoice with a matching ID
-# find_all_by_customer_id - returns either [] or one or more matches which have a matching customer ID
-# find_all_by_merchant_id - returns either [] or one or more matches which have a matching merchant ID
-# find_all_by_status - returns e
+ 
