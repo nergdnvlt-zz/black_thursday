4a3804a9caa577396b76422621da9f2d1892e108
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-24 10:20:28 -0700

    Working on iteration 2 refactor per sales harness and relationship layer.

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -11,12 +11,16 @@ class Invoice
 
   def initialize(data, parent = nil)
     @id           = data[:id].to_i
-    @customer_id  = data[:customer_id]
-    @merchant_id  = data[:merchant_id]
-    @status       = data[:status]
+    @customer_id  = data[:customer_id].to_i
+    @merchant_id  = data[:merchant_id].to_i
+    @status       = data[:status].to_sym
     @created_at   = Time.parse(data[:created_at])
     @updated_at   = Time.parse(data[:updated_at])
     @invoice_repo = parent
+  end
+
+  def merchant
+    @invoice_repo.find_merchant_by_merchant_id(merchant_id)
   end
 
   def customer
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,19 +1,24 @@
 require 'CSV'
 require_relative 'merchant_repository'
 require_relative 'item_repository'
+require_relative 'invoice_repository'
 
 # Creates a sales engine class to tie everything together
 class SalesEngine
   attr_reader :item_csv,
               :merchant_csv,
+              :invoice_csv,
               :items,
-              :merchants
+              :merchants,
+              :invoices
 
   def initialize(data)
     @item_csv = data[:items]
     @merchant_csv = data[:merchants]
+    @invoice_csv = data[:invoices]
     @items = ItemRepository.new(@item_csv, self)
     @merchants = MerchantRepository.new(@merchant_csv, self)
+    @invoices = InvoiceRepository.new(@invoice_csv, self)
   end
 
   def self.from_csv(data)
@@ -26,5 +31,9 @@ class SalesEngine
 
   def find_items_by_merchant_id(merchant_id)
     @items.find_all_by_merchant_id(merchant_id)
+  end
+
+  def find_merchant_by_merchant_id(id)
+    @merchants.find_by_id(id)
   end
 end
