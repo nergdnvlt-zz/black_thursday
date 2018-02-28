af010c263ec14016d55910f7cc708c9e1968e8fc
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-28 11:57:46 -0700

    Finished test error fix.

--- lib/best_invoices_module.rb
+++ lib/best_invoices_module.rb
@@ -25,48 +25,4 @@ module BestInvoices
     end
     highest[0]
   end
-
-  # def sort_by_quantity
-  #   quantities = {}
-  #   invoices.each do |invoice|
-  #     if invoice.is_paid_in_full?
-  #       quantities[invoice] = invoice.quantity_of_invoices
-  #     end
-  #   end
-  #   quantities
-  # end
-
-  # def best_invoice_by_quantity
-  #   high_quantity = sort_by_quantity.max_by do |_invoice, quantity|
-  #     quantity
-  #   end
-  #   high_quantity[0]
-  # end
-  # def best_invoice_by_revenue
-  #   revenues = {}
-  #   invoices.each do |invoice|
-  #     revenues[invoice] = invoice.total if invoice.is_paid_in_full?
-  #   end
-  #   highest_revenue = revenues.max_by do |_invoice, revenue|
-  #     revenue
-  #   end
-  #   highest_revenue[0]
-  # end
-  #
-  # def sorting_invoices_by_quantity
-  #   quantity_hash = {}
-  #   invoices.each do |invoice|
-  #     if invoice.is_paid_in_full?
-  #       quantity_hash[invoice] = invoice.quantity_of_invoices
-  #     end
-  #   end
-  #   quantity_hash
-  # end
-  #
-  # def best_invoice_by_quantity
-  #   high_quantity = sorting_invoices_by_quantity.max_by do |_invoice, quantity|
-  #     quantity
-  #   end
-  #   high_quantity[0]
-  # end
 end
